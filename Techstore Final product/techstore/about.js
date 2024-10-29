dfa1116045ccd1e06c73eb519decc3dbf4a2bbd3
Zain Ul Abdeen <zu4425@gmail.com>
2024-10-29 21:06:06 +0500

    Delete Techstore Final product directory

--- Techstore Final product/techstore/about.js
+++
@@ -1,9 +0,0 @@
-// Preloader functionality
-window.addEventListener('load', function () {
-    const preloader = document.getElementById('preloader');
-    if (preloader) {
-        preloader.style.display = 'none'; // Hide the preloader after the page is fully loaded
-    }
-});
-
-
